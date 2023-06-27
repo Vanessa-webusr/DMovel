031fc6f37fc5a5d62c7ca909cbbea7cebcf2d181
Vanessa-webusr <alvesnessa42@gmail.com>
2023-06-27 19:27:37 -0300

    alteração mapa e cor

---
+++ AtividadeAvaliativa1/app/src/main/java/com/example/atividadeavaliativa1/ui/map/MapFragment.java
@@ -0,0 +1,34 @@
+package com.example.atividadeavaliativa1.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.atividadeavaliativa1.databinding.FragmentMapBinding;
+
+public class MapFragment extends Fragment {
+
+    private FragmentMapBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        MapViewModel mapViewModel =
+                new ViewModelProvider(this).get(MapViewModel.class);
+
+        binding = FragmentMapBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
--- AtividadeAvaliativa1/app/src/main/java/com/example/atividadeavaliativa1/ui/perfil/PerfilFragment.java
+++ AtividadeAvaliativa1/app/src/main/java/com/example/atividadeavaliativa1/ui/perfil/PerfilFragment.java
@@ -1,4 +1,4 @@
-package com.example.atividadeavaliativa1.ui.dashboard;
+package com.example.atividadeavaliativa1.ui.perfil;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.atividadeavaliativa1.databinding.FragmentDashboardBinding;
+import com.example.atividadeavaliativa1.databinding.FragmentPerfilBinding;
 
-public class DashboardFragment extends Fragment {
+public class PerfilFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentPerfilBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        PerfilViewModel perfilViewModel =
+                new ViewModelProvider(this).get(PerfilViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentPerfilBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textPerfil;
+        perfilViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
