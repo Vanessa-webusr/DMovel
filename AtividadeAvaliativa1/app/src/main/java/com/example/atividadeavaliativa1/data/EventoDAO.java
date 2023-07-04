cff10c8ec50a7822e55cdb759c00bf893844b5b9
MatosAugusto <augustomatos@estudante.ufscar.br>
2023-07-04 19:09:10 -0300

    gets bd e repository

--- AtividadeAvaliativa1/app/src/main/java/com/example/atividadeavaliativa1/data/EventoDAO.java
+++ AtividadeAvaliativa1/app/src/main/java/com/example/atividadeavaliativa1/data/EventoDAO.java
@@ -2,10 +2,25 @@ package com.example.atividadeavaliativa1.data;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Transaction;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
 
 @Dao
 public interface EventoDAO {
+
+    @Query("SELECT * FROM evento ORDER BY id ASC")
+    public Flowable<List<Evento>> getAll();
     @Insert
-    void inserirEvento(Evento evento);
+    public Completable inserirEvento(Evento evento);
+
+    @Transaction
+    @Query("SELECT * FROM evento WHERE id = :id ORDER BY id ASC")
+    public Single<Evento> getEventoById(long id);
 }
 
