575185d844b6a6b27dfefec1bb5689555bb3c06f
Patrícia da Silva Ramos  Jujuba <56210981+PatriciaRamosS@users.noreply.github.com>
2023-07-03 22:51:32 -0300

    login e cadastro

---
+++ AtividadeAvaliativa1/app/src/main/java/com/example/atividadeavaliativa1/user/Activity_cadastro.java
@@ -0,0 +1,16 @@
+package com.example.atividadeavaliativa1.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.atividadeavaliativa1.R;
+
+public class Activity_cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
